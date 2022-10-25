c7fa55657ed9da1ec3b9f59861dd5cb2981a13fd
alim <7475149@mail.ru>
2022-10-25 12:01:41 +0600

    решена 3 задача

--- homework4.py
+++ homework4.py
@@ -46,18 +46,19 @@ count = raschet(a)                                            # вызываем
 
 '''
 
-# задача 3. Задана натуральная степень k. Сформировать случайным образом список коэффициенто
+# задача 3. Задана натуральная степень k. Сформировать случайным образом список коэффициенто...и записать в файл многочлен степени k
 
 from random import randint
-max_val=100
+max=100
 k = int(input('Введите натуральную степень k:'))
-
-koeff=[randint(0,max_val) for i in range(k)]+[randint(1,max_val)]
-poly='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(koeff) if j]   [::-1])
-
-
-poly=poly.replace('x^1+', 'x+')
-poly=poly.replace('x^0', '')
-poly+=('','1')[poly[-1]=='+']
-poly=(poly, poly[:-2])[poly[-2:]=='^1']
-print(poly)
+kof=[randint(0,max) for i in range(k)]+[randint(1,max)]
+pol='+'.join([f'{(j,"")[j==1]}x^{i}' for i, j in enumerate(kof) if j]   [::-1])
+pol=pol.replace('x^1+', 'x+')
+pol=pol.replace('x^0', '')
+pol+=('','1')[pol[-1]=='+']
+pol=(pol, pol[:-2])[pol[-2:]=='^1']
+print(pol)
+#-------------------------------------запись данных в файл по условиям задачи (и записать в файл многочлен степени k)------------------
+data = open('file.txt', 'w')
+data.writelines(pol)
+data.close()
